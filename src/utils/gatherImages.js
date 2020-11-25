78f58fd856e4a8b0635b3bbd8e424a3f1720fc06
Daniel Sneijers <daniel.sneijers@gmail.com>
2020-11-25 17:12:29 +0100

    exclude already converted files from gatherImages

--- src/utils/gatherImages.js
+++ src/utils/gatherImages.js
@@ -3,39 +3,42 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 const supportedFileFormat = [
-  '.jpg', 
-  '.jpeg',  
-  '.png', 
+  '.jpg',
+  '.jpeg',
+  '.png',
   '.webp',
-  '.heif', 
+  '.heif',
   '.heic',
   // no resizing of .svg files needed
 ];
 
-const prefixWith = prefix => file => `${prefix}/${file}`;
-const isDirectory = pathString => fs.lstatSync(pathString).isDirectory();
+const alreadyConvertedSuffixes = ['-small.', '-medium', '-large.'];
+
+const prefixWith = (prefix) => (file) => `${prefix}/${file}`;
+const isDirectory = (pathString) => fs.lstatSync(pathString).isDirectory();
 
 function gatherImages(inputDir) {
-  const allFiles = getFiles(inputDir)
+  const allFiles = getFiles(inputDir);
   const images = allFiles
-    .filter(file => supportedFileFormat.includes(path.extname(file)))
-    
+    .filter((file) => supportedFileFormat.includes(path.extname(file)))
+    .filter((file) => !alreadyConvertedSuffixes.some((suffix) => file.includes(suffix)));
+
   console.log(`${images.length} images found in ${chalk.green(inputDir)}\n`);
-  
+
   return images;
 }
 
 function getFiles(fromPath) {
   const prefixer = prefixWith(fromPath);
   const contents = fs.readdirSync(path.resolve(fromPath)).map(prefixer);
-  const directories = contents.filter(isDirectory); 
+  const directories = contents.filter(isDirectory);
 
   if (directories.length) {
-    const nestedContents = directories.map(dir => getFiles(dir))
-    return contents.concat(nestedContents).flat()
+    const nestedContents = directories.map((dir) => getFiles(dir));
+    return contents.concat(nestedContents).flat();
   }
-  
+
   return contents;
 }
 
-module.exports = gatherImages
+module.exports = gatherImages;
